40749459d33abe714287567396b4189edf8ce24e
Nese <neseilhannn@gmail.com>
2024-04-22 22:45:26 +0300

    Aritmetik comments added.

--- src/Aritmetik/aritmetik.java
+++ src/Aritmetik/aritmetik.java
@@ -5,9 +5,9 @@ public class aritmetik {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int sayi1,sayi2,sayi3;
+        int sayi1,sayi2,sayi3; //sayilar int olarak tanımlandı.
 
-        System.out.println("sayi 1'i giriniz: ");
+        System.out.println("sayi 1'i giriniz: "); //kullanıcıdan sayilari girmesini istiyoruz.
         sayi1 = input.nextInt();
 
         System.out.println("sayi 2'yi giriniz: ");
@@ -16,8 +16,9 @@ public class aritmetik {
         System.out.println("sayi 3'ü giriniz: ");
         sayi3 = input.nextInt();
 
-        double islem = (sayi1+sayi2*sayi3-sayi2);
-        System.out.println("Verdiginiz sayilara göre (a+b*c-b) isleminin sonucu: "  +islem);
+        double islem = (sayi1+sayi2*sayi3-sayi2); //bizim belirlediğimiz islem double tanımlandı.
+
+        System.out.println("Verdiginiz sayilara göre (a+b*c-b) isleminin sonucu: "  +islem); //islem sonucunu yazdırıyoruz.
 
     }
 }
