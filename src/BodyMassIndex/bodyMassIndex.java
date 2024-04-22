6d3a50d57c2c1592ca7ae6d83636f547bc2fa10b
Nese <neseilhannn@gmail.com>
2024-04-22 22:47:36 +0300

    bodyMassInd comments added.

--- src/BodyMassIndex/bodyMassIndex.java
+++ src/BodyMassIndex/bodyMassIndex.java
@@ -2,20 +2,21 @@ package BodyMassIndex;
 import java.util.Scanner;
 public class bodyMassIndex {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
-        double height,weight,bodyMassInd;
+        double height,weight,bodyMassInd; //boy kilo ve vucut endeksi double olarak tanımlandı.
 
 
-        System.out.println("Lütfen boyunuzu mt cinsinde giriniz (virgülle ayırınız): ");
+        System.out.println("Lütfen boyunuzu mt cinsinde giriniz (virgülle ayırınız): "); //kullanıcıdan boy istendi.
         height = input.nextDouble();
 
-        System.out.println("Lütfen kilonuzu (kg cinsinde) giriniz: ");
+        System.out.println("Lütfen kilonuzu (kg cinsinde) giriniz: "); //kullanıcıdan kilo istendi.
         weight = input.nextDouble();
 
-        bodyMassInd = height / weight * weight;
+        bodyMassInd = height / weight * weight; //vucut endeksi hesaplandı.
 
-        System.out.println("Vucut kitle endeksiniz: "+bodyMassInd);
+        System.out.println("Vucut kitle endeksiniz: "+bodyMassInd); //ekrana sonuc getirildi.
 
 
     }
