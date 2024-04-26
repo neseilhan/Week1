5b58e6962945ad9f73e55206a590c566a84235ea
Nese <neseilhannn@gmail.com>
2024-04-27 01:52:57 +0300

    upside_triangle completed.

---
+++ src/UpsideTriangle/upside_triangle.java
@@ -0,0 +1,19 @@
+package UpsideTriangle;
+import java.util.Scanner;
+
+public class upside_triangle {
+    public static void main(String[]Args){
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Bir sayi giriniz:"); //kullanicidan sayi girisi.
+        int num = inp.nextInt();
+
+        for(int i=num; i>=0; i--){  //girilen sayidan baslayip azalarak inen yildizlar.
+
+            for(int k=1; k<=(2*i+1); k++){ //1,3,5 seklinde giden yildizlar.
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
