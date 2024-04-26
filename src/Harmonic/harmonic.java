ed25dc051f6ed8a219bf500700be1c7374c35359
Nese <neseilhannn@gmail.com>
2024-04-26 16:36:30 +0300

    harmonic completed.

--- src/Harmonic/harmonic.java
+++ src/Harmonic/harmonic.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 public class harmonic {
 public static void  main(String[] Args){
 Scanner inp = new Scanner(System.in);
-System
-
+double result=0;
+System.out.println("Lutfen bir sayi griiniz: ");
+int n = inp.nextInt();
+for(double i=1; i<=n; i++){
+    result = result + (1/i); //ya da result = result + (1.0/i) yazabiliriz.
+}
+    System.out.println("Harmonik sayilar toplamı: "+result);
 
 }
 }
