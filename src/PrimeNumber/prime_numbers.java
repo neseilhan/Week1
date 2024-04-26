705a9f0eb71d8ba4805bc7c641591e352ccdaf66
Nese <neseilhannn@gmail.com>
2024-04-27 02:21:20 +0300

    prime_numbers completed.

---
+++ src/PrimeNumber/prime_numbers.java
@@ -0,0 +1,19 @@
+package PrimeNumber;
+
+public class prime_numbers {
+    public static void main(String[] Args) {
+
+        for (int i = 2; i <= 1000; i++) { //2den 100'e kadar olan sayilar
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false; //sadece asal olanlari göstermesi için kontrol degiskeni ekledik.
+//                    break;
+                }
+            }
+            if (isPrime) { //sayi asalsa yazdir.
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
