526821b6553dc281c76e948f6bcc84599e773b32
Nese <neseilhannn@gmail.com>
2024-04-26 23:39:45 +0300

    atm changed.

--- src/ATM/atm.java
+++ src/ATM/atm.java
@@ -8,6 +8,7 @@ public class atm {
         int balance = 10; //bankadaki paramiz.
         int select;
         int money=0;
+        Boolean logout = true;
         System.out.println("-----Gringotts Bankasına Hosgeldiniz------");
 
         do {
@@ -17,53 +18,56 @@ public class atm {
             System.out.println("Lutfen sifre giriniz:");
             password = inp.nextLine();
 
+
             //sifre ve kullanici adi kontrolu
             if (userName.equals("Nese") && password.equals("kod123")) { //eslesmesi kontrol edildi.
                 System.out.println("Giris Basarili.");
+                if (logout) {
 
-                //islem secimi
-                System.out.println("Lutfen yapmak istediginiz islemi seciniz: ");
-                System.out.println(" 1-Para Yatırma\n 2-Para Cekme\n 3-Bakiye Sorgulama\n 4-Cikis Yap ");
-                select = inp.nextInt();
+                    //islem secimi
+                    System.out.println("Lutfen yapmak istediginiz islemi seciniz: ");
+                    System.out.println(" 1-Para Yatırma\n 2-Para Cekme\n 3-Bakiye Sorgulama\n 4-Cikis Yap ");
+                    select = inp.nextInt();
 
-                switch (select) {
-                    case 1:
-                        System.out.println("Lutfen yatirmak istediginiz miktari giriniz. ");
-                        money = inp.nextInt();
-                        balance += money;
-                        System.out.println("Hesabinizdaki yeni miktar: " + balance);
+                    switch (select) {
+                        case 1:
+                            System.out.println("Lutfen yatirmak istediginiz miktari giriniz. ");
+                            money = inp.nextInt();
+                            balance += money;
+                            System.out.println("Hesabinizdaki yeni miktar: " + balance);
 
-                        break;
-                    case 2:
-                        System.out.println("Lutfen cekmek istediginiz miktari giriniz. ");
-                        if (money > balance) {
-                            System.out.println("Bakiye yetersiz.");
-                        } else {
-                            balance -= money;
-                        }
-                        System.out.println("Hesabinizdaki guncel miktar: " + balance);
-                        break;
-                    case 3:
-                        System.out.println("Guncel bakiyeniz :  " + balance);
-                        break;
-                    case 4:
-                        System.out.println("Cikis yaptiniz.  ");
-                        break;
-                    default:
-                        System.out.println("Hatali giris yaptiniz.");
-                        break;
+                            break;
+                        case 2:
+                            System.out.println("Lutfen cekmek istediginiz miktari giriniz. ");
+                            if (money > balance) {
+                                System.out.println("Bakiye yetersiz.");
+                            } else {
+                                balance -= money;
+                            }
+                            System.out.println("Hesabinizdaki guncel miktar: " + balance);
+                            break;
+                        case 3:
+                            System.out.println("Guncel bakiyeniz :  " + balance);
+                            break;
+                        case 4:
+                            System.out.println("Cikis yaptiniz.  ");
+                            logout = false;
+                            break;
+                        default:
+                            System.out.println("Hatali giris yaptiniz.");
+                            break;
+                    }
                 }
 
-            }
-            else {
-                heart--;
-                System.out.println("Hatalı kullanıcı adı veya şifre. Kalan hakkınız : "+heart);
-            }
-            if (heart == 0) {
-                System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
-            }
+                } else {
+                    heart--;
+                    System.out.println("Hatalı kullanıcı adı veya şifre. Kalan hakkınız : " + heart);
+                }
+                if (heart == 0) {
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+                }
 
-        } while(heart>0);
+        } while(heart>0 && logout);
     }
 
 }
