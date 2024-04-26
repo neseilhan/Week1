f032b28a09ea1d6586b66fe2f7954daaa20ad2d4
Nese <neseilhannn@gmail.com>
2024-04-27 00:41:07 +0300

    ebob-ekok2 completed.

---
+++ src/EbobEkok2/ebob_ekok2.java
@@ -0,0 +1,27 @@
+package EbobEkok2;
+import java.util.Scanner;
+public class ebob_ekok2 {
+    public static void main(String[] Args){
+        Scanner inp = new Scanner(System.in);
+        int num1,num2;
+        int ebob=1;
+        int ekok=1;
+        int i=1;
+
+        System.out.println("Lutfen birinci sayiyi giriniz :");
+        num1 = inp.nextInt();
+
+        System.out.println("Lutfen ikinci sayiyi giriniz :");
+        num2 = inp.nextInt();
+
+        while(i <= num1 && i<=num2){
+            if(num1%i ==0 && num2%i==0){
+                ebob = i;
+            }
+            i++;
+        }
+        ekok = (num1*num2)/ebob;
+        System.out.println("Bu sayilarin EBOB'u :"+ebob);
+        System.out.println("Bu sayilarin EKOK'u :"+ekok);
+    }
+}
