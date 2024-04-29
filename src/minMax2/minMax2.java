f9343acba568564e548fd3204928ef6e863e03e0
Nese <neseilhannn@gmail.com>
2024-04-29 15:28:09 +0300

    [another way of minmax problem]

---
+++ src/minMax2/minMax2.java
@@ -0,0 +1,38 @@
+package minMax2;
+import java.util.Scanner;
+
+public class minMax2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Hesaplanacak sayı adedini kullanıcıdan al:
+        System.out.print("Hesaplanacak sayı adedini girin (n): ");
+        int count = scanner.nextInt();
+
+        if (count <= 0) {
+            System.out.println("Girilen sayı pozitif tam sayı olmalıdır: ");
+            return;
+        }
+        //Kullancıdan ilk sayıyı al:
+        System.out.print("İlk sayıyı girin: ");
+        double num = scanner.nextDouble();
+        double min = num;
+        double max = num;
+        //İlk sayı girildiği için döngüyü 2'den başlat ve (n)'e kadar sayı girişi iste:
+        for (int i = 2; i <= count; i++) {
+            System.out.print("Sayı " + i + ": ");
+            num = scanner.nextDouble();
+            //Eğer sayı minimumdan küçükse yeni değeri o sayıya eşitle:
+            if (num < min) {
+                min = num;
+            }
+            //Eğer sayı maksimumdan büyükse yeni değeri o sayıya eşitle:
+            if (num > max) {
+                max = num;
+            }
+        }
+        System.out.println("En kücük sayı: " + min);
+        System.out.println("En büyük sayı: " + max);
+
+        scanner.close();
+    }
+}
